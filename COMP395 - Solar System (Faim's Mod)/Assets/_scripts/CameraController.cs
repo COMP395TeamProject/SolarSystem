399d3d51148226304f2b775ac29b815246109cb3
Faim Khandker <faim.khandker@gmail.com>
2017-02-22 22:18:29 -0500

    Commit 7
    Combined Version - Seth added gravity script onto asteroid, Seth changed the scale, positioning and the rigidbody of the planets

---
+++ COMP395 - Solar System (Faim's Mod)/Assets/_scripts/CameraController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.UI;
+
+public class CameraController : MonoBehaviour 
+{
+	//Instances
+//	public Camera camera;
+//	public Slider zoom;
+
+	void Start()
+	{
+		//Referrencing to the component in unity
+//		GetComponent<Camera> ();
+	}
+
+	//update is called per frame
+	void Update()
+	{
+		//conditional to "zoom in"
+		if (Input.GetAxis ("Mouse ScrollWheel") > 0) 
+		{
+			//Referrencing to the component in unity
+			GetComponent<Camera>().fieldOfView--; //decrementing the "fieldOfView" so we can look at the scene closer
+		}
+
+		//conditional to "zoom out"
+		if (Input.GetAxis ("Mouse ScrollWheel") < 0) 
+		{
+			//Referrencing to the component in unity
+			GetComponent<Camera>().fieldOfView++; //incrementing the "fieldOfView" so we can look away from the scene
+		}
+	}
+
+	//Methods
+//	public void ResetCameraPosition()
+//	{
+//		camera.transform (0, 100, -100);
+//		camera.fieldOfView (60);
+//	}
+}
---
+++ COMP395 - Solar System (Faim's Mod)/Assets/_scripts/UIController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour 
+{
+
+	//Public Instances
+	//NOTE: These instances refers to the "txt" counterpart compoenents due to me adding the button component on the "txt"s
+	public Button btnBegin;
+	public Button btnOverview;
+	public Button btnMenu;
+	public Button btnExit;
+	public Button btnReset;
+
+	//Initialization
+	public void Start()
+	{
+		//Initializing instance varaibles to corresponding components
+		//		btnBegin = btnBegin.GetComponent<Button>();
+		//		btnInfo = btnInfo.GetComponent<Button>();
+		//		btnExit = btnExit.GetComponent<Button>();
+	}
+
+	//Public Methods
+	//begins simulation by changing to "SimulationScene"
+	public void BeginSimulation()
+	{
+		SceneManager.LoadScene ("SimulationScene");
+	}
+
+//	//Change scene to "OverviewScene"
+//	public void ChangeToOverviewScene()
+//	{
+//		SceneManager.LoadScene ("OverviewScene");
+//	}
+
+	//Change scene to "MenuScene"
+	public void BackToMenuScene()
+	{
+		SceneManager.LoadScene ("MenuScene");
+	}
+
+	//Quites Game
+	public void Reset()
+	{
+		SceneManager.LoadScene ("SimulationScene");
+	}
+
+	//Quites Game
+	public void Exit()
+	{
+		Application.Quit();
+	}
+}
